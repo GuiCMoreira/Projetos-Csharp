5c8d3cf98369673df8a8c63340e09c8d2d1d4d15
Guilherme Carvalho <gui0307carvalho@gmail.com>
2025-02-24 20:18:23 -0300

    Removendo Projeto

--- Introducao/Introducao/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Introducao;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
