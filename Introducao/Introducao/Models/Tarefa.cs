0ca7bb30e312b430160e0372b16aa027b364e343
Guilherme Carvalho <gui0307carvalho@gmail.com>
2025-02-24 20:31:30 -0300

    Update Tarefa.cs

--- Introducao/Introducao/Models/Tarefa.cs
+++ Introducao/Introducao/Models/Tarefa.cs
@@ -8,5 +8,12 @@ namespace Introducao.Models
 {
     public class Tarefa
     {
+        private string nome;
+        private string descricao;
+        private DateTime inserida;
+        private DateTime? finalizada;
+        private bool pronto;
+        private int codigo;
+        private static int id = 0;
     }
 }
