797fc2af30ad67e3e6f9b000924e7f4035c79727
Guilherme Carvalho <gui0307carvalho@gmail.com>
2025-02-24 20:20:53 -0300

    Iniciando Projeto

---
+++ Introducao/Introducao/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Introducao;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
