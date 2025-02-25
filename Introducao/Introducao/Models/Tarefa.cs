b22896a7554dd93388e5528199589e390f729d44
Guilherme Carvalho <gui0307carvalho@gmail.com>
2025-02-24 21:09:59 -0300

    Update Tarefa.cs

--- Introducao/Introducao/Models/Tarefa.cs
+++ Introducao/Introducao/Models/Tarefa.cs
@@ -11,9 +11,24 @@ namespace Introducao.Models
         private string nome;
         private string descricao;
         private DateTime inserida;
-        private DateTime? finalizada;
+        private DateTime? concluida;
         private bool pronto;
         private int codigo;
         private static int id = 0;
+
+        public Tarefa()
+        {
+            id++;
+            codigo = id;
+            inserida = DateTime.Now;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Descricao { get => descricao; set => descricao = value; }
+        public DateTime Inserida { get => inserida; }
+        public DateTime? Concluida { get => concluida; }
+        public bool Pronto { get => pronto; set => pronto = value; }
+        public int Codigo { get => codigo; }
+        public static int Id { get => id; }
     }
 }
